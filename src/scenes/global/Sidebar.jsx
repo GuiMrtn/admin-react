409972b4c38bbc0b3e9848c4a910065ff0affba6
Martin Piat <mrtn.piat@gmail.com>
2023-02-05 19:02:03 +0100

    wip

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar@0.7.1";
+//import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+
+const Sidebar = () => {
+    return <div>Sidebar</div>
+}
+
+export default Sidebar;
