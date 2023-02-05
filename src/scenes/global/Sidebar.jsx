6bc8831d5d2d7c66d78e844b829ff6a453a8e95e
Martin Piat <mrtn.piat@gmail.com>
2023-02-05 19:27:27 +0100

    init, topbar, router done

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar@0.7.1";
 //import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme} from "@mui/material"
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 
 const Sidebar = () => {
     return <div>Sidebar</div>
